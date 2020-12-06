2ba3e08646d9789b6fa17e7a4f549a471f763b69
MichaelxTruong <71095329+MichaelxTruong@users.noreply.github.com>
2020-12-06 14:36:05 -0500

    Create Assignment4Page3.js

---
+++ Assignment4Page3.js
@@ -0,0 +1,4 @@
+var favouritecolour = prompt ("What is your favourite colour?");
+var output = "Welcome to the" + " " + favouritecolour + " " + "team!";
+
+document.getElementById("prompt").innerHTML = output;
